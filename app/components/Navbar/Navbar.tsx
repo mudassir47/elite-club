07684e0466ef420499222163c1a10089927e71a7
mudassir47 <mkteams472@gmail.com>
2024-09-02 22:05:23 -0700

    done

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,116 @@
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import React from 'react';
+import Drawer from "./Drawer";
+import Drawerdata from "./Drawerdata";
+import Signdialog from "./Signdialog";
+import Registerdialog from "./Registerdialog";
+import Image from 'next/image';
+
+
+interface NavigationItem {
+    name: string;
+    href: string;
+    current: boolean;
+}
+
+const navigation: NavigationItem[] = [
+    { name: 'Home', href: '/', current: true },
+    { name: 'About', href: '#services', current: false },
+    { name: 'Team', href: '#about', current: false },
+    { name: 'Certificate', href: '#project', current: false },
+    { name: 'Gallery', href: '/', current: false },
+]
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
+
+const Navbar = () => {
+
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    return (
+        <Disclosure as="nav" className="navbar">
+            <>
+                <div className="mx-auto max-w-7xl px-6 lg:py-4 lg:px-8">
+                    <div className="relative flex h-20 items-center justify-between">
+                        <div className="flex flex-1 items-center sm:items-stretch sm:justify-start">
+
+                            {/* LOGO */}
+
+                            <div className="flex flex-shrink-0 items-center">
+    <div className="block lg:hidden">
+        <Image
+            src="/assets/logo/logo.png"
+            alt="Elit"
+            width={130} // Adjust the width as needed
+            height={38} // Adjust the height as needed
+        />
+        
+    </div>
+    <div className="hidden lg:block">
+        <Image
+            src="/assets/logo/logo.png"
+            alt="Elit"
+           
+            width={120} // Set desired width
+            height={41} // Set desired height
+        />
+    </div>
+</div>
+
+                            {/* LINKS */}
+
+                            <div className="hidden lg:block m-auto">
+                                <div className="flex space-x-4">
+                                    {navigation.map((item) => (
+                                        <Link
+                                            key={item.name}
+                                            href={item.href}
+                                            className={classNames(
+                                                item.current ? ' text-black hover:opacity-100' : 'hover:text-black hover:opacity-100',
+                                                'px-3 py-4 text-lg font-normal opacity-75 space-links'
+                                            )}
+                                            aria-current={item.href ? 'page' : undefined}
+                                        >
+                                            {item.name}
+                                        </Link>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* SIGNIN DIALOG */}
+
+                        <Signdialog />
+
+
+                        {/* REGISTER DIALOG */}
+
+                        <Registerdialog />
+
+
+                        {/* DRAWER FOR MOBILE VIEW */}
+
+                        {/* DRAWER ICON */}
+
+                        <div className='block lg:hidden'>
+                            <Bars3Icon className="block h-6 w-6" aria-hidden="true" onClick={() => setIsOpen(true)} />
+                        </div>
+
+                        {/* DRAWER LINKS DATA */}
+
+                        <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
+                            <Drawerdata />
+                        </Drawer>
+
+                    </div>
+                </div>
+            </>
+        </Disclosure>
+    )
+}
+
+export default Navbar;
